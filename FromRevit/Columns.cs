698a0f1d32b9184c85191cb88a8278009c3d04f1
AhmedLGaiar <ahmedlgaiar@gmail.com>
2025-03-14 15:06:47 +0200

    Add Revit add-in project "FromRevit"
    This commit introduces a new Revit add-in project named "FromRevit". It includes a Visual Studio solution file (`CSIxRevit.sln`), a project file (`FromRevit.csproj`), and an add-in manifest file (`app.addin`). The project features a command that interacts with Revit's API to retrieve column information, implemented in the `Columns` class which adheres to the `IExternalCommand` interface. Assembly attributes in `AssemblyInfo.cs` provide metadata for the add-in, and necessary Revit API libraries are referenced. A post-build event is also included to copy the output DLL to the Revit add-ins directory.

---
+++ FromRevit/Columns.cs
@@ -0,0 +1,35 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace FromRevit
+{
+    [Transaction(TransactionMode.ReadOnly)]
+    public class Columns : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            try
+            {
+                UIDocument uIDocument = commandData.Application.ActiveUIDocument;
+
+                Document document = uIDocument.Document;
+
+                return Result.Succeeded;
+
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                return Result.Failed;
+
+            }
+        }
+    }
+}
